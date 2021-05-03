bfcedad3725aabd32cb9e719bafa6c3e4e92c8e5
Ivana Tosheva <toshevaivana1995@gmail.com>
2021-05-03 21:29:47 +0200

    node-schedule-and-node-cron

---
+++ project/controllers/blog-posts.js
@@ -0,0 +1,68 @@
+const {blogPostModel} = require('../models/blog-post')
+const successResponse = require('../lib/success-response-sender');
+const errorResponse = require('../lib/error-response-sender');
+const nodemailer = require('../lib/nodeMailer');
+
+module.exports = {
+  fetchAll: async (req, res) => {
+    try {
+      const blogPosts = await blogPostModel.find().populate('category', 'name').populate('user')
+      successResponse(res, 'List of all blog posts', blogPosts);
+    } catch (error) {
+      errorResponse(res, 500, error.message)
+    }
+  },
+  fetchOne: async (req, res) => {
+    console.log(req.user);
+    try {
+      const blogPost = await blogPostModel.findById(req.params.id).populate('category', 'name')
+      if (!blogPost) errorResponse(res, 400, 'No user with the provided id')
+
+      successResponse(res, `Post with id #${req.params.id}`, blogPost);
+    } catch (error) {
+      errorResponse(res, 500, error.message)
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const blogPost = await blogPostModel.create(req.body);
+      if (blogPost) { nodemailer() }
+    
+      successResponse(res, 'New blog post created', blogPost);
+    } catch (error) {
+      errorResponse(res, 500, error.message)
+    }
+  },
+  patchUpdate: async (req, res) => {
+    try {
+      const blogPost = await blogPostModel.findByIdAndUpdate(req.params.id, req.body)
+      successResponse(res, 'Blog post updated', blogPost);
+    } catch (error) {
+      errorResponse(res, 500, {
+        ...req.body,
+        _id: req.params.id,
+        error: error.message
+      })
+    }
+  },
+  putUpdate: async (req, res) => {
+    try {
+      const blogPost = await blogPostModel.findOneAndReplace({ _id: req.params.id }, req.body)
+      successResponse(res, 'Blog post updated', blogPost);
+    } catch (error) {
+      errorResponse(res, 500, {
+        ...req.body,
+        _id: req.params.id,
+        error: error.message
+      })
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      await blogPostModel.remove({ _id: req.params.id });
+      res.send(`BlogPost ${req.params.id} is deleted`);
+    } catch (error) {
+      res.send({ message: error });
+    }
+  }
+}
